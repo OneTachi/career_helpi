5da694c3d232c4dec1dd2329dfd775973b74389f
ChristopherRasquin <crasquin@udel.edu>
2024-04-02 17:07:40 -0400

    AddedName

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ function App() {
   }
   return (
     <div className="App">
+      <div>Christopher Rasquin</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
