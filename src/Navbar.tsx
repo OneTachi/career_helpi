d139247d2b1175ffca3865a0282ecdbebfcf3c10
sidd <n2verz@gmail.com>
2024-04-09 15:58:14 -0400

    fixing esling errors

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
-import logo from "./assets/images/spider-logo.png"
 import "./assets/css/Navbar.css"
 import { Button } from 'react-bootstrap';
-import { PageProps, Page } from "./interfaces/page"
+import { PageProps } from "./interfaces/page"
 
 export function Navbar({page, setPage} : PageProps) : JSX.Element
 {
