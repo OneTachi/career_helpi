9611fc1665f508e098056a255af35002d7b82b69
sidd <n2verz@gmail.com>
2024-04-17 17:20:29 -0400

    create LoadPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
 import React, { useState } from "react";
 import "./assets/css/App.css";
 import { Navbar } from "./Navbar";
-import { Page } from "./interfaces/page";
+import { Page, PageProps } from "./interfaces/page";
 import { Footer } from "./Footer";
 import { Homepage } from "./Homepage";
 
 function App() {
-  const [page, setpage] = useState<Page>("home");
-  return (
-    <body>
-      <Navbar page={page} setPage={setpage}></Navbar>
-      <Homepage page={page} setPage={setpage}></Homepage>
-      <Footer page={page} setPage={setpage}></Footer>
-    </body>
-  );
+  const [page, setPage] = useState<Page>("home");
+
+  return LoadPage({ page, setPage });
+}
+
+function LoadPage({ page, setPage }: PageProps): JSX.Element {
+  switch (page) {
+    case "home": {
+      return (
+        <body>
+          <Navbar page={page} setPage={setPage}></Navbar>
+          <Homepage page={page} setPage={setPage}></Homepage>
+          <Footer page={page} setPage={setPage}></Footer>
+        </body>
+      );
+    }
+    case "basic": {
+      return <body>Basic Page Layout</body>;
+    }
+    case "detailed": {
+      return <body>Detailed Page Layout</body>;
+    }
+    case "results": {
+      return <body>Results Page Layout</body>;
+    }
+  }
+
+  return <div>ERROR</div>;
 }
 
 export default App;
