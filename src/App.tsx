4f9a8fa0348ea210a45f8b6d113d5c6d6a62b06c
sidd <n2verz@gmail.com>
2024-04-17 17:32:08 -0400

    Document LoadPage & App

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,19 @@ import { Footer } from "./Footer";
 import { Homepage } from "./Homepage";
 
 function App() {
+  // Hook used for page of website. Does NOT handle individual questions in basic/detailed pages.
   const [page, setPage] = useState<Page>("home");
 
   return LoadPage({ page, setPage });
 }
 
+/**
+ * Displays appropriate section of the website (page) to the user
+ * @param { page, setPage} : PageProps => Page state to pass onto UI Components
+ * @returns Page shown to user
+ */
 function LoadPage({ page, setPage }: PageProps): JSX.Element {
+  // Add JSX of page to corresponding page below.
   switch (page) {
     case "home": {
       return (
@@ -32,7 +39,7 @@ function LoadPage({ page, setPage }: PageProps): JSX.Element {
       return <body>Results Page Layout</body>;
     }
   }
-
+  // This error should never occur.
   return <div>ERROR</div>;
 }
 
