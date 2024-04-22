f7969f3c1108c2f821c3a570081554677a12a8a2
ChristopherRasquin <crasquin@udel.edu>
2024-04-21 21:14:04 -0400

    Aligned Basic and Detailed Questions

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -11,7 +11,7 @@ export function Homepage({ page, setPage }: PageProps): JSX.Element {
         className="basic-button"
         onClick={() => setPage("basic")}>Basic Questions</Button>
         <br />
-        <p>
+        <p className="basic-text">
           Our basic quiz is a multiple choice assessment that determines your
           strengths to find what you would be best at. Remember, there are no
           wrong answers. Follow your heart, and you will get the results that
@@ -23,7 +23,7 @@ export function Homepage({ page, setPage }: PageProps): JSX.Element {
         className={"detailed-button"} 
         onClick={() => setPage("detailed")}>Detailed Questions</Button>
         <br />
-        <p>
+        <p className="detailed-text">
           Our complex quiz consists of open response boxes, sliders, and other
           inputs. It allows you to truly unleash your inner creativity and be
           yourself! You can answer questions with anything you want, and do what
