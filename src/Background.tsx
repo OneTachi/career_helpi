d216c5154b1d8bf8b488691da761bb8c54200ccc
jaburger <jaburger@udel.edu>
2024-05-02 14:23:55 -0400

    Added new assets for results page (background and background web)

--- src/Background.tsx
+++ src/Background.tsx
@@ -10,6 +10,7 @@ import { Results } from './Results';
 import forest from "./assets/images/backgrounds/forest.png"
 import insideHouse from "./assets/images/backgrounds/insideHouse.png"
 import waterfall from "./assets/images/backgrounds/waterfall.gif"
+import garden from "./assets/images/backgrounds/garden.png"
 
 
 interface changePageProps{
@@ -47,13 +48,13 @@ export function Background({quizType}: {quizType: string}): JSX.Element{
     return(
         <div onMouseMove={updateCompletionAmount}>
             {quizType === "results" ? 
-                <Results fields={["a", "b", "c"]} jobs={[["a1", "a2", "a3"], ["b1", "b2", "b3"], ["c1", "c2", "c3"]]} descriptions={[["ad1", "ad2", "ad3"], ["bd1", "bd2", "bd3"], ["cd1", "cd2", "cd3"]]}></Results> 
+                <Results fields={["a aaaaaaaaaaaaa a a a", "b", "c"]} jobs={[["a1", "a2", "a3"], ["b1", "b2", "b3"], ["c1", "c2", "c3"]]} descriptions={[["ad1", "ad2", "ad3"], ["bd1", "bd2", "bd3"], ["cd1", "cd2", "cd3"]]}></Results> 
                 : quizType === "basic" ?
                     <MultipleChoice question={questions[pageNumber - 1]} answers={answers[pageNumber - 1]} pageNum = {pageNumber} selectedAnswers={selectedAnswers} changeAnswer={changeAnswer} completionAmount = {completionAmount} changeCompletionAmount={changeCompletionAmount}></MultipleChoice> 
                     : <div>Detailed</div>
             }
             
-            {quizType !== "results" ? <img src = {backgrounds[pageNumber - 1]} alt = "Background Img" className="Background-Image"></img> : <img src = {forest} alt = "Background Img" className="Background-Image"></img>}
+            {quizType !== "results" ? <img src = {backgrounds[pageNumber - 1]} alt = "Background Img" className="Background-Image"></img> : <img src = {garden} alt = "Results Background Img" className="Background-Image"></img>}
             {quizType !== "results" ? <ChangePage pageNumber={pageNumber} changePageNumber={changePageNumber}></ChangePage> : ""}
             {quizType !== "results" ? <ProgressBar amountCompleted={completionAmount}></ProgressBar> : ""}
         </div>
--- src/Results.tsx
+++ src/Results.tsx
@@ -4,7 +4,7 @@ import spider0 from "./assets/images/characters/main/idle (main).gif";
 import spider1 from "./assets/images/characters/orange/idle (orange).gif";
 import spider2 from "./assets/images/characters/blue/idle (blue).gif";
 
-
+import backgroundWeb from "./assets/images/objects/result web.png";
 import miniWeb0 from "./assets/images/objects/progress bar/progressWeb0.png"; //Delete me!!!!
 
 interface selectedFieldProps{
@@ -38,6 +38,7 @@ interface selectedJobProps{
 export function Results({fields, jobs, descriptions}: {fields: string[], jobs: string[][], descriptions: string[][]}): JSX.Element{
     const [selectedField, changeSelectedField] = useState<string>("");
     const [selectedJob, changeSelectedJob] = useState<string>("");
+    const [showWeb, changeShowWeb] = useState<boolean>(false)
 
     function updateSelectedField(newField: string){
         changeSelectedField(newField);
@@ -55,6 +56,7 @@ export function Results({fields, jobs, descriptions}: {fields: string[], jobs: s
     return(
         <div>
             <div onClick={() => updateSelectedField("")} className="Results-Click-Box"></div>
+            <img src = {backgroundWeb} alt = "Background Img" onClick={() => updateSelectedField("")} style={{width: "100%", height: "100%", position: "absolute", opacity: (Number(showWeb) * 100).toString() + "%"}} onMouseOver={()=> changeShowWeb(true)} onMouseOut={()=> changeShowWeb(false)}></img>
             {fields.map((field: string) => <CareerSpider field={field} jobs={jobs[fields.indexOf(field)]} descriptions={descriptions[fields.indexOf(field)]} selectedField={selectedField} updateSelectedField={updateSelectedField} selectedJob={selectedJob} updateSelectedJob={updateSelectedJob} spiderNum={fields.indexOf(field)}></CareerSpider>)}
         </div>
     );
@@ -90,35 +92,3 @@ export function JobFly({job, description, selectedJob, updateSelectedJob}: selec
         </div>
     );
 }
-
-
-//The following works for getting functional clicks to show the jobs of a certian field when you click on that field's spider, but I believe it won't work when clicking on jobs themselves due to this resulting in another mouseDown that would close the currently opened field
-/*
-    return(
-        <div onMouseDown={() => updateSelectedField("")} className="Results-Click-Box">
-            {fields.map((field: string) => <CareerSpider field={field} jobs={jobs[fields.indexOf(field)]} descriptions={descriptions[fields.indexOf(field)]} selectedField={selectedField} updateSelectedField={updateSelectedField} selectedJob={selectedJob} updateSelectedJob={updateSelectedJob} spiderNum={fields.indexOf(field) + 1}></CareerSpider>)}
-        </div>
-    );
-}
-
-
-export function CareerSpider({field, jobs, descriptions, selectedField, updateSelectedField, selectedJob, updateSelectedJob, spiderNum}: selectedFieldProps): JSX.Element{
- return(
-        <div onMouseUp={() => updateSelectedField(field)} key={field} className={"Career-Spider Spider-" + spiderNum.toString()}>
-            <h3 className={"Field-Text"}>{field}</h3>
-            <img src = {mainSpider} alt = "result spider img" className="Spider-Image"></img>
-            {field === selectedField && jobs.map((job: string) => <JobFly job={jobs[jobs.indexOf(job)]} description={descriptions[jobs.indexOf(job)]} selectedJob={selectedJob} updateSelectedJob={updateSelectedJob}></JobFly>)}
-        </div>
-    );
-}
-
-export function JobFly({job, description, selectedJob, updateSelectedJob}: selectedJobProps): JSX.Element{
-    return(
-        <div onMouseUp={() => updateSelectedJob(job)} key={job} style = {{backgroundColor: "purple"}} className="Job-Fly">
-            <h2 className={"Job-Text"}>{job}</h2>
-            <img src = {miniWeb0} alt = "test img" style = {{left: "50%", top: "50%"}} className = "Fly-Image"></img>
-            <div>{description}</div>
-        </div>
-    );
-}
-*/
