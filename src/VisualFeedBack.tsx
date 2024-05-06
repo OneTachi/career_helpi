9805159908f527378177a137c5beea1f044839d2
ChristopherRasquin <crasquin@udel.edu>
2024-05-05 22:01:02 -0400

    bringing branch up to date: Christopher Rasquin

---
+++ src/VisualFeedBack.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import {Button} from "react-bootstrap";
+
+export function VisualFeedback(): JSX.Element{
+    // This is the State (Model)
+    const [submitted, setSubmitted] = useState<boolean>(false);
+
+    // This is the Control
+    function updateSubmitted(event: React.ChangeEvent<{}>) {
+        //im assuming that once we have the submit questions button operational
+        //that will be the event it references
+        setSubmitted(true)
+      }
+    // This is the View
+    return <div>
+
+      {<Button onClick={updateSubmitted} hidden={submitted}>Submit Answers</Button>}
+
+    <div hidden={!submitted}>Your Answers have been submitted To ChatGPT</div>
+
+
+    </div>
+}
