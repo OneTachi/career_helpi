2604cd90155ec000e16d4a3c985830f12525b2b1
sidd <n2verz@gmail.com>
2024-05-07 15:40:35 -0400

    handle attribution testing

--- src/tests/Api.test.tsx
+++ src/tests/Api.test.tsx
@@ -1,17 +1,53 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "../App";
-import { handleResponseAttribution } from "../Api";
+import { handleResponseAttribution, getAttributes } from "../Api";
+
+let quiz_format: Record<string, number> = {
+  "problem solving": 0,
+  protectiveness: 0,
+  creativity: 0,
+  empathy: 0,
+  leadership: 0,
+  communication: 0,
+  teamwork: 0,
+  patience: 0,
+  organization: 0,
+  "decision making": 0,
+  adaptability: 0,
+  independence: 0,
+  ethics: 0,
+  analytics: 0,
+};
 
 describe("handleResponseAttribution Tests", () => {
   test("Handles correct case", () => {
+    // Purposefully does not reset quiz_format for next question
     render(<App />);
     handleResponseAttribution("ethics, teamwork, empathy", "basic");
+    quiz_format["ethics"] = 1;
+    quiz_format["teamwork"] = 1;
+    quiz_format["empathy"] = 1;
+    expect(getAttributes("basic")).toEqual(quiz_format);
   });
-});
-
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  test("Does not increment mispelled text", () => {
+    render(<App />);
+    handleResponseAttribution("ethic", "basic");
+    expect(getAttributes("basic")).toEqual(quiz_format);
+    quiz_format["ethics"] = 0;
+    quiz_format["teamwork"] = 0;
+    quiz_format["empathy"] = 0;
+  });
+  test("Increment by two", () => {
+    render(<App />);
+    handleResponseAttribution("ethics, ethics", "basic");
+    quiz_format["ethics"] = 2;
+    expect(getAttributes("basic")).toEqual(quiz_format);
+    quiz_format["ethics"] = 0;
+  });
+  test("Handles empty case", () => {
+    render(<App />);
+    handleResponseAttribution("", "basic");
+    expect(getAttributes("basic")).toEqual(quiz_format);
+  });
 });
