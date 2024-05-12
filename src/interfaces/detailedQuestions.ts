dfade2e4815570d4f3ec6aa0f173d35894afeb97
sidd <n2verz@gmail.com>
2024-05-12 15:16:29 -0400

    finish Q2 points

--- src/interfaces/detailedQuestions.ts
+++ src/interfaces/detailedQuestions.ts
@@ -13,4 +13,17 @@ export const detailedQuestions: string[] = [
   "Stickiness",
 ];
 
-export const detailedPoints: { attributes: string[]; points: number[] }[] = [];
+export const detailedPoints: { attributes: string[]; points: number[] }[] = [
+  { attributes: ["patience, creativity"], points: [1, 1] },
+  { attributes: ["communication, decision making"], points: [1, 1] },
+  { attributes: ["adaptability, analytics"], points: [1, 1] },
+  { attributes: ["independence, organization"], points: [1, 1] },
+  { attributes: ["protectiveness"], points: [1] },
+  { attributes: ["independence"], points: [1] },
+  { attributes: ["problem solving"], points: [1] },
+  { attributes: ["creativity"], points: [1] },
+  { attributes: ["adaptability"], points: [1] },
+  { attributes: ["ethics"], points: [1] },
+  { attributes: ["analytics"], points: [1] },
+  { attributes: ["patience"], points: [1] },
+];
